020a8026772fca0cbd40bbe8110c89ba56fda6d1
Joel Hyyppä <joel.hyyppa@gmail.com>
2022-11-08 16:21:00 +0200

    header changes

--- samatpelit/components/Header.js
+++ samatpelit/components/Header.js
@@ -3,10 +3,10 @@ import styles from '../styles/header.module.css';
 export default function Header() {
   return (
     <header className={styles.header}>
-        <h1> Samat Pelit </h1>
-        <h1> Burgermenu </h1>
-        <h1> Joo </h1>
-        <h1> Joo </h1>
-    </header>
+        <button className={styles.button}> Login </button>
+        <button className={styles.button}> Help </button><h1> Samat Pelit </h1>
+        <h2>Virkistäytymispelejä kaikille opiskelijoille!</h2>
+      </header>
+    
   )
 }
