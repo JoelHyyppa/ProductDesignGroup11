610dbbcba13f4804c62e1a075da96c47c47463b4
Niko Tuulaniemi <niko.tuulaniemi@gmail.com>
2022-11-08 14:54:29 +0200

    Started to do layouts

---
+++ samatpelit/components/Header.js
@@ -0,0 +1,12 @@
+import styles from '../styles/header.module.css';
+
+export default function Header() {
+  return (
+    <header className={styles.header}>
+        <h1> Samat Pelit </h1>
+        <h1> Burgermenu </h1>
+        <h1> Joo </h1>
+        <h1> Joo </h1>
+    </header>
+  )
+}
--- samatpelit/pages/index.js
+++ samatpelit/pages/index.js
@@ -1,6 +1,7 @@
 import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
+import Header from '../components/Header'
 
 export default function Home() {
   return (
@@ -11,7 +12,25 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <main className={styles.main}>
+      <Header/>
+
+      <footer className={styles.footer}>
+        <a
+          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Powered by{' '}
+          <span className={styles.logo}>
+            <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
+          </span>
+        </a>
+      </footer>
+    </div>
+  )
+}
+
+/*<main className={styles.main}>
         <h1 className={styles.title}>
           Welcome to <a href="https://nextjs.org">Next.js!</a>
         </h1>
@@ -52,20 +71,4 @@ export default function Home() {
             </p>
           </a>
         </div>
-      </main>
-
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <span className={styles.logo}>
-            <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
-          </span>
-        </a>
-      </footer>
-    </div>
-  )
-}
+      </main>*/
