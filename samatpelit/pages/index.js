9a9b6128a081f2b2e057a9f9d73ac764fbfe9dc9
Niko Tuulaniemi <niko.tuulaniemi@gmail.com>
2022-11-09 14:19:41 +0200

    Added jsconfig.json to add module aliases. Also added 'children' -prop to layout so it works now.

--- samatpelit/pages/index.js
+++ samatpelit/pages/index.js
@@ -1,11 +1,9 @@
-import Image from 'next/image'
-import styles from '../styles/Home.module.css'
-import Layout from '../components/Layout'
+import Layout from '@/components/Layout'
 
 export default function Home() {
   return (
-    <div className={styles.container}>
-      <Layout/>
-    </div>
+      <Layout>
+        <h1> Home </h1>
+      </Layout>
   )
 }
