1cfe0271355efc63f7f470814a10965748410b03
Niko Tuulaniemi <niko.tuulaniemi@gmail.com>
2022-11-09 13:45:30 +0200

    Made layout component and added it to every existed page.

--- samatpelit/pages/games/[game].js
+++ samatpelit/pages/games/[game].js
@@ -1,9 +1,12 @@
 import { useRouter } from 'next/router'
+import Layout from '../../components/Layout'
 
 export default function GamePage() {
     const router = useRouter()
   return (
-    <div> <h1> This is game page</h1>
+    <div> 
+        <Layout/>
+        <h1> This is game page</h1>
     <h2>game = {router.query.game}</h2></div>
   )
 }
