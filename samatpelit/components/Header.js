eb5d45c1d13e227a942764c4560448659e28c350
Joel Hyyppä <joel.hyyppa@gmail.com>
2022-11-09 12:48:05 +0200

    searchbar added

--- samatpelit/components/Header.js
+++ samatpelit/components/Header.js
@@ -4,8 +4,10 @@ export default function Header() {
   return (
     <header className={styles.header}>
         <button className={styles.button}> Login </button>
-        <button className={styles.button}> Help </button><h1> Samat Pelit </h1>
+        <button className={styles.button}> Help </button>
+        <h1> Samat Pelit </h1>
         <h2>Virkistäytymispelejä kaikille opiskelijoille!</h2>
+        <p> <input class="searchBar" type="text" placeholder="Etsi peliä..." /> </p>
       </header>
     
   )
