11f9a0d89a8891ef91c973cd1a25de38b5a9023b
Joel Hyyppä <joel.hyyppa@gmail.com>
2022-11-09 15:39:10 +0200

    sekoilu

--- samatpelit/components/Header.js
+++ samatpelit/components/Header.js
@@ -1,10 +1,12 @@
 import styles from "@/styles/header.module.css"
+import Button from "./Button"
 
 export default function Header() {
   return (
     <header className={styles.header}>
-      <button className={styles.button}> Kirjaudu </button>
-      <button className={styles.button}> Pelit </button>
+    <div><Button>Kirjaudu</Button>
+    <Button>Pelit</Button>
+    <Button>Rekisteröidy</Button></div>
       <h1> Samat Pelit </h1>
       <h2>Virkistäytymispelejä kaikille opiskelijoille!</h2>
       <p>
