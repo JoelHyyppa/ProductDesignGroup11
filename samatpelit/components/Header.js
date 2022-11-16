6d650bb327cd0d594dad6afef5a8f35ab4a7bd88
Niko Tuulaniemi <niko.tuulaniemi@gmail.com>
2022-11-16 15:22:52 +0200

    Logo now navigates to homepage on clicked.

--- samatpelit/components/Header.js
+++ samatpelit/components/Header.js
@@ -2,16 +2,21 @@ import styles from "@/styles/header.module.css"
 import Logo from "./Logo"
 import Button from "./Button"
 import Search from "./Search"
+import Link from "next/link"
 
 export default function Header() {
   return (
     <header className={styles.header}>
-    <div><Button>Kirjaudu</Button>
-    <a href="/games"><Button>Pelit</Button></a>
-    <Button>Rekisteröidy</Button></div>
       <Logo />
       <h2>Virkistäytymispelejä kaikille opiskelijoille!</h2>
       <Search />
+      <div>
+        <Button>Kirjaudu</Button>
+        <a href="/games">
+          <Button>Pelit</Button>
+        </a>
+        <Button>Rekisteröidy</Button>
+      </div>
     </header>
   )
 }
--- samatpelit/components/Logo.js
+++ samatpelit/components/Logo.js
@@ -1,9 +1,12 @@
 import styles from "@/styles/logo.module.css"
+import Link from "next/link"
 
 export default function Logo() {
   return (
     <div className={styles.logo}>
-      <h1>Samat Pelit</h1>
+      <Link href="/">
+        <h1>Samat Pelit</h1>
+      </Link>
     </div>
   )
 }
