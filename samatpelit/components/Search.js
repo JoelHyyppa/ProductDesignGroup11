589c610c75b27bcbb3339477cb4e393a711d02d6
Niko Tuulaniemi <niko.tuulaniemi@gmail.com>
2022-11-16 15:08:36 +0200

    reformatting

--- samatpelit/components/Search.js
+++ samatpelit/components/Search.js
@@ -1,10 +1,12 @@
-import styles from '@/styles/Search.module.css'
+import styles from "@/styles/Search.module.css"
 
 export default function Search() {
-    return <div className={styles.search}>
-    <p>
+  return (
+    <div className={styles.search}>
+      <p>
         {" "}
         <input class="searchBar" type="text" placeholder="Etsi peliä..." />{" "}
       </p>
-      </div>
-  }
+    </div>
+  )
+}
--- samatpelit/pages/games/index.js
+++ samatpelit/pages/games/index.js
@@ -1,6 +1,6 @@
 import GameGrid from "@/components/GameGrid"
 import Layout from "@/components/Layout"
-import styles from '../../styles/header.module.css'
+import styles from "../../styles/header.module.css"
 import Button from "@/components/Button"
 import Logo from "@/components/Logo"
 
@@ -8,16 +8,24 @@ export default function GamesPage() {
   return (
     <div>
       <header className={styles.header}>
-    <div><Button>Kirjaudu</Button>
-    <a href="/"><Button>Home Page</Button></a>
-    <Button>Rekisteröidy</Button></div>
-      <Logo />
-      <h2>Virkistäytymispelejä kaikille opiskelijoille!</h2>
-      <p>
-        {" "}
-        <input class="searchBar" type="text" placeholder="Etsi peliä..." />{" "}
-      </p>
-    </header>
+        <div>
+          <Button>Kirjaudu</Button>
+          <a href="/">
+            <Button>Home Page</Button>
+          </a>
+          <Button>Rekisteröidy</Button>
+        </div>
+        <Logo />
+        <h2>Virkistäytymispelejä kaikille opiskelijoille!</h2>
+        <p>
+          {" "}
+          <input
+            class="searchBar"
+            type="text"
+            placeholder="Etsi peliä..."
+          />{" "}
+        </p>
+      </header>
       <GameGrid />
     </div>
   )
