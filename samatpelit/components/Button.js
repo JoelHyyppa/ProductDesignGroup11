34877ec811e27ec6380f93d1b477a06937c08b6b
Niko Tuulaniemi <niko.tuulaniemi@gmail.com>
2022-11-18 15:00:44 +0200

    Modified Button-Component to send onClick signal when pressed. Headers' buttons now navigate uses onClick and router.push to navigate to coresponding page. Added cursor:pointer to button hover and game grid item hover.

--- samatpelit/components/Button.js
+++ samatpelit/components/Button.js
@@ -1,5 +1,18 @@
 import styles from "@/styles/Button.module.css"
 
-export default function Button({ children }) {
-  return <button className={styles.button}>{children}</button>
+export default function Button({ children, variant, type, onClick }) {
+  return (
+    <button
+      onClick={onClick}
+      type={type}
+      className={styles[`variant-${variant}`]}
+    >
+      {children}
+    </button>
+  )
+}
+
+Button.defaultProps = {
+  variant: "primary",
+  type: "button",
 }
