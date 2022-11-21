e72d332bd1f957037f225bd09e61d904c62388c3
Joel Hyyppä <joel.hyyppa@gmail.com>
2022-11-21 15:14:49 +0200

    register dialog works

--- samatpelit/components/RegisterModal.js
+++ samatpelit/components/RegisterModal.js
@@ -1,24 +1,30 @@
 import styles from '@/styles/Modal.module.css'
-import LoginPage from 'pages/account/login'
 import { useState, useEffect } from 'react'
 import ReactDOM from 'react-dom'
 import {FaTimes} from 'react-icons/fa'
-import Button from './Button'
+import RegisterPage from 'pages/account/register'
 
-export default function Modal({show, onClose, children, title} ){
+export default function RegisterModal({show, onClose, children, title} ){
 
  const [isBrowser, setIsBrowser] = useState(false)
  useEffect(() => setIsBrowser(true)) 
 
+ const handleClose = (event) => {
+   
+    ({onClose}) ()
+
+    event.preventDefault ();
+ }
+
  const modalContent = show ? (
     <div className={styles.modal}>
     <div className={styles.header}>
-    <Button type="button" onClick={onClose}>
-          <FaTimes />
-        </Button>
+        <a href="/" onClick={handleClose}>
+            <FaTimes />
+        </a>
     </div>
     {title && <div>{title}</div>}
-    <div className={styles.body}><LoginPage /></div>
+    <div className={styles.body}><RegisterPage /></div>
     </div>
  ) : null
   
