f46b913bee6b442cee721a4c4b9a6141bf913b90
Riku Korkiasaari <74648156+KorkiasaariRiku@users.noreply.github.com>
2022-11-22 15:38:43 +0200

    Main pagen ulkoasun muokkauksia

--- samatpelit/pages/index.js
+++ samatpelit/pages/index.js
@@ -1,9 +1,29 @@
+import GameGrid from "@/components/GameGrid"
 import Layout from "@/components/Layout"
+import styles from "@/styles/Home.module.css"
+import Image from 'next/image'
+import alcohol from '../assets/alcohol.jpg'
 
 export default function Home() {
-  return (
+  return(
+  <main className={styles.Home}>
     <Layout>
-      <h1> Home </h1>
+      <div className={styles.etusivu}>
+      
+        <Image
+            src={alcohol}
+            alt='Picture of alcohol'
+            width= {700}
+            height= {700}
+className={styles.kuva}
+        />
+        <aside >
+          <GameGrid />
+        </aside>
+      
+
+      </div>
     </Layout>
+    </main>
   )
 }
