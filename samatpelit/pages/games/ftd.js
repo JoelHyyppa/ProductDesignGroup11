89c3bd6f0594ecfe4a60958a2c72f8ae3a588bfc
Riku Korkiasaari <74648156+KorkiasaariRiku@users.noreply.github.com>
2022-11-28 16:10:13 +0200

    code styling

--- samatpelit/pages/games/ftd.js
+++ samatpelit/pages/games/ftd.js
@@ -1,14 +1,13 @@
-import { useState } from "react";
-import Layout from "@/components/Layout";
-import scrambledDeck from "@/components/Deck";
+import { useState } from "react"
+import Layout from "@/components/Layout"
+import scrambledDeck from "@/components/Deck"
 
 export default function App() {
-
-    const [deck, setDeck] = useState([]);
-    const getDeck = () => {
-        setDeck(scrambledDeck())
-    };
-    /*const generate = () => {
+  const [deck, setDeck] = useState([])
+  const getDeck = () => {
+    setDeck(scrambledDeck())
+  }
+  /*const generate = () => {
       //we run this generate function with a button
       //inside this function, we call Math.random to generate a number between 0 and 1
       //then we multiply that with truthOrDare.length and then round that down
@@ -17,28 +16,20 @@ export default function App() {
       setIndex(index);
     };*/
 
-    /*const test = () => {
+  /*const test = () => {
         //var deckk = ['AS', 'AH', 'AD', 'AC']
         //setDeck(scramble(deckk))
         setDeck(scrambledDeck())
     }*/
 
-    const card = deck.map((item) => (
-        <li
-          key={item}
-        >
-            {item}
-        </li>
-      ))
+  const card = deck.map((item) => <li key={item}>{item}</li>)
 
-    return (
-      <div className="App">
-                <Layout />
-        <button onClick={getDeck}>Nosta kortti</button>
-        <p>{deck}</p>
-        <ul>
-        {card}
-        </ul>
-      </div>
-    );
+  return (
+    <div className="App">
+      <Layout />
+      <button onClick={getDeck}>Nosta kortti</button>
+      <p>{deck}</p>
+      <ul>{card}</ul>
+    </div>
+  )
 }
