d6cf959d36af204218b02c6c0adc3bfef7559ea6
Joel Hyyppä <joel.hyyppa@gmail.com>
2022-12-05 13:42:29 +0200

    help page

--- samatpelit/pages/help.js
+++ samatpelit/pages/help.js
@@ -5,6 +5,15 @@ export default function AboutPage() {
     <div>
       <Layout />
       <h1> Help page </h1>
+      <div>Tämä sivusto sisältää erilaisia opiskelijoiden illanviettoihin suunnattuja pelejä.</div>
+      <div>Pelivalikoimaamme kuuluu Hitler, Truth or Dare sekä Fuck The Dealer.</div>
+      <div>Hitler on perinteinen korttipeli, jossa pelaajat nostavat kortteja vuorollaan.</div>
+      <div>Säännöt ovat vaihtelevia, mutta perinteisesti korttinumeroiden 4 ja 5 kohdalla jokainen huutaa "HITLER". Viimeisenä huutanut pääsee ottamaan huikkaa.</div>
+      <div>Tarkemmat ohjeet löytyvät sivulta https://www.juomapelit.net/hitler, mutta tietyn kortin nostettaessa tapahtuvaa asiaa voidaan muuttaa sen mukaan, miten porukka on tottunut pelaamaan peliä.</div>
+      <div>Truth or Dare eli totuus ja tehtävä-pelissä, kuten nimikin kertoo, kerrotaan totuuksia tai tehdään tehtäviä, mitä kohdalle osuu.</div>
+      <div>Fuck the Dealer on arvauspeli, jossa jakaja kysyy ensimmäiseltä (yleensä vasemmalla) olevalta pelaajalta, mikä kortti on pakan päällä.</div>
+      <div>Pelissä voidaan arvata kortin maata, väriä, tai arvoa (suurempi, pienempi, ulkona, välistä).</div>
+      <div>Jos pelisivustollamme ilmenee ongelmia, ota yhteyttä Niko Tuulaniemeen, Riku Korkiasaareen tai Joel Hyyppään. </div>
     </div>
   )
 }
